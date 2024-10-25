11d75a30dfcc7b23ada8bb493854654f579796ae
iam-prasanna-dcloud <prasanna4win@gmail.com>
2024-10-25 19:56:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
-
+    return "Here's a brief description of your project:Flask Docker Application Pipeline: This project automates the deployment of a simple Flask application using Docker. The pipeline is configured in Jenkins to clone the application's code from a GitHub repository, build a Docker image based on a specified Dockerfile, and then push the image to Docker Hub. It leverages a declarative Jenkins pipeline to streamline the Continuous Integration/Continuous Deployment (CI/CD) process, ensuring that the application is always up-to-date and easily deployable. This setup enhances development efficiency by enabling quick iterations and testing in isolated environment"
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
